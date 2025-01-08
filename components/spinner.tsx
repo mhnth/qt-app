9404f59b4b3f96b30bbd181ae25afec9f32759b6
manhnth <n.t.manh@viscirclevn.com>
2025-01-08 17:24:02 +0700

    update 8

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,3 +1,3 @@
 export const Spinner = () => (
-  <div className="h-6 w-6 animate-spin rounded-full border-4 border-grey-300 border-t-grey-50"></div>
+  <div className="h-6 w-6 animate-spin rounded-full border-4 border-gray-500 border-t-gray-50"></div>
 );
